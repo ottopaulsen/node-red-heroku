2dd69da00329ba3396c138abb4cb651f651187e2
Otto Paulsen <otto.paulsen@itema.no>
2016-10-03 19:23:13 +0200

    Postinstall script to generate cred file

---
+++ scripts/postinstall.sh
@@ -0,0 +1,14 @@
+# Create the flows_cred.json file with credentials from environment variables
+
+FLOWS_CRED_FILE=.node-red/flows_cred.json
+cat > $FLOWS_CRED_FILE << EOF
+{
+    "1183f03b.fae04": {
+        "user": "${MQTT_USER}",
+        "password": "${MQTT_PASSWORD}"
+    },
+    "d56617a5.79e518": {
+        "apiKey": "${THINGSPEAK_API_KEY}"
+    }
+}
+EOF
